62956a83020c6d4c831bbb1cfcab9ef0ec707512
M Tanzim Rahman <tanzimr59@gmail.com>
2026-09-14 18:24:02 +0000

    class practice

---
+++ week_1/practice/class_1.py
@@ -0,0 +1,10 @@
+def my_info(name, dept, Id, number):
+    print("I am " + name)
+    print("Departmaent: " + dept  )
+    print("ID:" + Id)
+    print("Phone:" + number);
+name = input("Enter name: ")
+dept = input("Enter dept: ")
+Id = input("Enter ID: ")
+number = input("Enter number: ")
+my_info(name, dept, Id, number)
--- week_1/practice/fun_project1/main.py
+++ week_1/practice/fun_project1/main.py
@@ -1,5 +1,16 @@
 import game
 games=[]
 
-game.add_Games(games)
-game.display_Games(games)
+# game.add_Games(games)
+# game.display_Games(games)
+while True: 
+    choice = input("Choose from the menue: " + "\n"+ "1. Add Game" + "\n" + "2. Display Games" + "\n" + "3. Exit" + "\n" )
+    if(choice=="1" or choice=="Add Game"):
+        game.add_Games(games)
+    elif(choice=="2" or choice=="Display Games"):
+        game.display_Games(games)
+    elif(choice=="3" or choice=="Exit"):
+        break
+    else:
+        print("Invalid Choice")
+
