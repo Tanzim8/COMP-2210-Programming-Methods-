74ada4dc3cb5171d286e5ddd57d2eb94798f69d1
M Tanzim Rahman <tanzimr59@gmail.com>
2026-09-18 18:14:31 +0000

    practice

--- week_1/practice/class.py
+++ week_1/practice/class.py
@@ -8,3 +8,26 @@ print(num4)
 num6 = int(input())
 num7 = num6-num4
 print(int(num7))
+
+print(5==4)
+
+a = 5
+b = 4
+
+print("a(5) and b(4) are equal")
+print(a==b)
+
+print("a(5) is greater than  b(4)")
+print(a>b)
+
+print("a(5) is smaller than b(4)")
+print(a<b)
+
+c = 6
+
+print("a(5) is greater or equal to c(6)")
+print(a>=c)
+
+print("a(5) is smaller or equal to c(6)")
+print(a<=c)
+
---
+++ week_1/practice/class_Task.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter num 1: "))
+num2 = int(input("Enter num 2: "))
+num3 = int(input("Enter num 3: "))
+if(num1>num2 and num2 > num3):
+    print("num1 value is greater than num3 value")
+if(num1<num2 and num2<num3):
+    print("num1 value is smaller than num3 value")
